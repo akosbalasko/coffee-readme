a57763cc0091acde536019e8015b36914e2e3e1d
Akos Balasko <akos0215@gmail.com>
2022-03-22 22:32:17 +0100

    remove unused files

--- src/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface CoffeeSupporter {
-  support_note: string
-  support_coffees: number
-  payer_name: string
-}
-export interface CoffeeSupportersResponse {
-  data: CoffeeSupporter[]
-}
